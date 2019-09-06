297fb66c1acd90550780831c76e22cd15778d1f2
Jewgenii <jewgenijerochin@gmail.com>
2019-09-06 17:40:22 +0300

    restfull crud

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -15,6 +15,7 @@ public class UserController {
 
     @Autowired
     public PersonService person_service;
+    public Person p;
 
     @RequestMapping(name = "/add",method = RequestMethod.PUT)
     public Person add(@RequestParam String first_name,String second_name,String email,Long age) {
@@ -28,10 +29,29 @@ public class UserController {
         person_service.save(p);
         return p;
     }
+
     @RequestMapping(name = "/delete",method = RequestMethod.DELETE)
     public Person delete(@RequestParam Long id){
-        Person p = person_service.findById(id).get();
+        p = person_service.findById(id).get();
         person_service.deleteById(id);
     return p;
     }
+
+    @RequestMapping(name = "/update",method = RequestMethod.POST)
+    public Person update(@RequestParam Long id,String first_name,String second_name,String email,Long age){
+        p = person_service.findById(id).get();
+        p.setSecond_name(second_name);
+        p.setFirst_name(first_name);
+        p.setEmail(email);
+        p.setAge(age);
+
+        person_service.save(p);
+        return p;
+    }
+    @RequestMapping(name="/get",method = RequestMethod.GET)
+    public Person get(@RequestParam Long id){
+        Optional<Person> p = person_service.findById(id);
+        return p.get();
+    }
+
 }
